56b9309b7de3b04e7802bf7c8fa92871041de4f3
unknown <kurniantolucky9@gmail.com>
2024-09-29 11:07:53 +0700

    another try resolving svg can't be build

--- src/components/cards/EmptyCard.jsx
+++ src/components/cards/EmptyCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import noData from "../../assets/noData.svg";
+import notFound from "../../assets/search-not-found.svg"
 
-const EmptyCard = ({imgSrc, message}) => {
+
+const EmptyCard = ({type, message}) => {
   return (
     <div className='flex flex-col justify-center items-center px-4 py-20 bg-white'>
-      <img src={imgSrc} alt="No data" className='w-96 '/>
+      <img src={type == 'noData' ? noData : notFound} alt="No data" className='w-96 '/>
       <p className='w-1/2 text-sm  font-medium text-slate-700 text-center leading-7 mt-5'>{message}</p>
     </div>
   )
